a79281a67d58ff3f9f84c6f790160ae733557cf0
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-01-26 01:56:36 +0800

    Minor changes on home page

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -2,8 +2,8 @@ const Maintenance = () => {
   return (
     <div className=" grid place-items-center mb-[10em]">
         <div className=" text-[5rem] text-center">
-            <h1>404 Not Found</h1>
-            <p className=" mt-20">This page is still on work...</p>
+            <h1 className=" leading-[1em]">404 Not Found</h1>
+            <p className=" mt-20 leading-[1em] text-[1.5rem] lg:text-[5rem]">This page is still on work...</p>
         </div>
     </div>
   )
